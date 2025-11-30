dd2b98d3aa97132a3fdb92e7df52543b697736b5
Tellapuri Hari Kiran Reddy <harikiranreddytellapuri@gmail.com>
2025-11-30 19:42:47 +0530

    Update-2

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const Cart = require('./Cart');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: [ 'http://localhost:5174', "http://localhost:5173"],
+  origin: [ 'http://localhost:5174', "http://localhost:5173", "https://tharikiranreddy-ecommerce-application.vercel.app"],
   credentials: true,
 }));
 
